f405d6da3190d19dd959a4b4ec18b835cbeefe99
julianstalter <78112604+julianstalter@users.noreply.github.com>
2023-12-21 16:15:46 +0100

    neo4j connection

--- knowgl/neo4j/neofourj.py
+++ knowgl/neo4j/neofourj.py
@@ -2,10 +2,11 @@ from neo4j import GraphDatabase
 import json
 
 uri = "bolt://localhost:7687"  # Change to Neo4j instance URI
-username = "neo4j"             # Change to your username
-password = "password"          # Change to your password
+username = "neo4j"  # Change to your username
+password = "leonardo"  # Change to your password
 
 driver = GraphDatabase.driver(uri, auth=(username, password))
+
 
 def add_triplet(tx, subject, predicate, object):
     query = (
@@ -15,14 +16,21 @@ def add_triplet(tx, subject, predicate, object):
     )
     tx.run(query, subject=subject, predicate=predicate, object=object)
 
+
 def import_triplets(file_path):
     with driver.session() as session:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             for line in file:
                 triplet = json.loads(line)
-                session.write_transaction(add_triplet, triplet['subject'], triplet['predicate'], triplet['object'])
+                session.write_transaction(
+                    add_triplet,
+                    triplet["subject"],
+                    triplet["predicate"],
+                    triplet["object"],
+                )
+
 
 # Replace
-import_triplets('path/to/your/file.jsonl')
+import_triplets("./data.jsonl")
 
 driver.close()
