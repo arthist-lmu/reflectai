7ff5c06d50076cbd0ffab2d7c70d269024ad5c86
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-03-28 16:15:26 +0100

    tate data import

--- knowgl/data_import/tate.py
+++ knowgl/data_import/tate.py
@@ -4,25 +4,24 @@ import json
 import uuid
 
 # {
-#     "url": "https:\/\/en.wikipedia.org\/wiki\/Scene_in_the_Northwest:_Portrait_of_John_Henry_Lefroy",
-#     "title": "Scene in the Northwest: Portrait of John Henry Lefroy",
+#     "url": "https://www.tate.org.uk/art/artworks/zoffany-three-sons-of-john-3rd-earl-of-bute-t07863",
+#     "title": "Three Sons of John, 3rd Earl of Bute",
 #     "text": {
-#         "entry": "Scene in the Northwest: Portrait of John Henry Lefroy, also known as The Surveyor, is a painting by Paul Kane circa 1845. It sold at auction in 2002 for C$5.1 million, making it the most expensive Canadian painting ever sold at that time. It was purchased by media magnate Ken Thomson, who donated it to the Art Gallery of Ontario. The painting depicts British explorer John Henry Lefroy on his successful expedition to map the Magnetic North Pole. Lefroy returned to Toronto in November 1844 and it is likely that Kane painted him soon after that. Lefroy helped convince George Simpson to fund Kane's own western expeditions, which Kane began in June 1845. In the painting, Lefroy is dressed in the outfit of a coureur des bois wearing snow shoes, standing in front of a dog sled. In the background one of his companions walks towards a native woman's tipi. Lefroy returned to England in 1853 and took the painting with him. The original remained in the Lefroy family for some 150 years, but they had no knowledge of the artist or its value. It was forgotten in Canada until researchers at the Library and Archives Canada and the Art Gallery of Ontario learned of its existence and located it in the possession of one of Lefroy's descendants.[1] They opted to put it up for auction in 2002. It sold at auction in February 2002 at Sotheby's Canada in Toronto for $5,062,500. The painting was appraised at $450,000 to $550,000, but a competitive auction vastly exceeded its appraised value as Thomson's agent competed with two American bidders.[2] Its sale price more than doubled the previous record for a Canadian painting\u2014$2.2 million for Lawren Harris's Baffin Island, which had been bought by Thomson the previous year. The sale price of Scene in the Northwest was almost ten times more than had ever had been paid for a Kane work; the previous record was $525,000 for Portrait of Maungwudaus in 1999.[3] While scholars had no knowledge of the original painting for over a century, a copy of the painting did remain in Canada. This version was acquired by the Glenbow Museum in Calgary in the 1950s. Its artist, and who was depicted, had been forgotten, leading to many decades of debate. Some scholars credited the Glenbow painting to Kane and others to Cornelius Krieghoff. The rediscovery of the much higher-quality original has led to the consensus opinion that it is a copy of Kane's piece done by a lesser artist of the same period. One possibility is that it was created by Kane's wife, Harriet Clench, herself a skilled painter."
+#         "entry": "John Stuart, 3rd Earl of Bute, commissioned this portrait of three of his sons as a pair with one of three of his daughters (Tate T07864). It was painted not long after Zoffany had left his native Germany for England in 1760. Zoffany quickly established himself in London as the leading painter of lively conversation pieces such as this, which were greatly admired for their charming informality.\nIn his portraits of the Three Sons and Three Daughters of Lord Bute, the only known pair of portraits of children by Zoffany, the artist stresses the playful and mischievous aspect of childhood, yet succeeds in capturing the youngsters’ charm without undue sentiment.\nThe Earl of Bute was one of Zoffany’s earliest and most important patrons. As well as this portrait and its pair, he commissioned a full-length of his heir, John, Lord Mountstuart, later 1st Marquess of Bute, in Masquerade Dress c.1763-4 (private collection). The 3rd Earl was a great patron of the arts and formed an important collection, including paintings, prints and books. He was best-known as a close advisor and later favourite Prime Minister (1762–3) to the young King George III, although he resigned from that position in 1763, at around the time the Bute portraits were painted. Nevertheless, he remained one of the most powerful aristocrats in Britain. It was probably Lord Bute who, following this commission, introduced Zoffany to the King and Queen, initiating a highly successful period of Royal patronage for the artist.\nIn this animated portrait the three younger sons of the Earl have abandoned their archery in favour of birdnesting. On the left is the Hon. William Stuart (1755–1822) who reaches up to his brother to collect the bird and eggs in his black tricorn hat. The Hon. Charles Stuart (1753–1801) sits rather precariously up in the branches of the oak as he raids the nest. To the right is the Hon. Frederick Stuart (1751–1802) who clutches his bow and proudly points to the bull’s eye he has just scored on the distant target. William and Charles went on to have illustrious public careers: William subsequently became Archbishop of Armagh, while Charles was later made Lieutenant-General the Hon. Sir Charles Stuart, who captured Minorca from the Spaniards in 1798.\nThe compositions of both pendant portraits are carefully contrived geometric designs. Here, the pyramidal arrangement complements the diagonal composition used for the sisters’ portrait, who are shown playing with pet squirrels. The landscape setting for both paintings is the park at Luton Hoo, Bedfordshire, which had become the Bute family seat in 1763. The artist has paid close attention to detail and texture, particularly in the delicate foliage, the gnarled trunk of the tree and in the treatment of the poodle, which waits in hope for the spoils of the boys’ sport. The two paintings mark the beginning of a more sophisticated approach by Zoffany to group portraiture, the figures being given prominence over the landscape setting. The Bute portraits can be compared with the slightly earlier pair of conversation pieces that Zoffany produced for his other important patron, the actor David Garrick, both entitled Mr and Mrs Garrick at Hampton Gardens 1762 (Lord Lambton), in which the smaller, less assured figures are placed in more expansive landscapes.\nAll six children in the portraits are treated in a lively and affectionate manner with convincing personalities. Comparable earlier eighteenth-century images of children tended to be more stiff and formal, emphasising their dynastic importance in maintaining an aristocratic lineage, rather than their individual natures. Zoffany’s portraits of children anticipated a change in attitude towards children, who were beginning to be seen in a more sentimental light, in keeping with contemporary notions of sensibility. Zoffany’s ability to capture their energy and individuality can be seen in several of his portraits of children, including those of the royal family (Royal Collection). He seems to have had a genuine sympathy with children and their games and amusements that gives an attractive sincerity to his pictures of them.\n\nFurther reading\nLady Victoria Manners and G.C. Williamson, Johan Zoffany R.A. His Life and Works 1735–1810, 1920, p.186.Sacheverell Sitwell, Conversation Pieces, London 1936, p.39, reproduced pl. 48.Mary Webster, Johan Zoffany 1733–1810, exhibition catalogue, National Portrait Gallery, London 1977, no.20, reproduced.\nDiane PerkinsNovember 2003"
 #     },
-#     "images": [
-#         "https:\/\/upload.wikimedia.org\/wikipedia\/commons\/2\/24\/Kane_The_Surveyor.jpg"
-#     ],
+#     "images": ["https://www.tate.org.uk/art/images/work/T/T07/T07863_10.jpg"],
 #     "language": "EN",
 # }
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Transfer tate data to a common format"
+        description="Transfer rijksmuseum data to a common format"
     )
 
     parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
     parser.add_argument("-i", "--input_path", help="verbose output")
+    parser.add_argument("-o", "--output_path", help="verbose output")
 
     args = parser.parse_args()
     return args
@@ -30,6 +29,55 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    results = []
+    with open(args.input_path) as f:
+        for line in f:
+            line_data = json.loads(line)
+            line_id = uuid.uuid5(uuid.NAMESPACE_URL, line_data["url"]).hex
+
+            language = line_data["language"]
+            if isinstance(language, (list, set)):
+                for l in language:
+                    if l.lower() == "en":
+                        language = "en"
+
+                if language != "en":
+                    print(f"Unknown language: {line_data}")
+                    exit(1)
+
+            results.append(
+                {
+                    "id": line_id,
+                    "meta": {"url": line_data["url"]},
+                    "text": [
+                        {
+                            "content": line_data["title"],
+                            "page": 0,
+                            "type": "title",
+                            "language": language.lower(),
+                        },
+                        {
+                            "content": line_data["text"]["entry"],
+                            "page": 0,
+                            "type": "text",
+                            "language": language.lower(),
+                        },
+                    ],
+                    "images": [
+                        {
+                            "url": x,
+                            "page": 0,
+                            "id": uuid.uuid5(uuid.NAMESPACE_URL, x).hex,
+                        }
+                        for x in line_data.get("images", [])
+                    ],
+                }
+            )
+
+    with open(args.output_path, "w") as f:
+        for r in results:
+            f.write(json.dumps(r) + "\n")
 
     return 0
 
