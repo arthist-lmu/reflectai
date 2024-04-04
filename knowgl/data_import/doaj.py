3b6610c07bfb087aae26ff0c8f31c4abd290c002
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-04-04 16:46:18 +0200

    doaj language fix

--- knowgl/data_import/doaj.py
+++ knowgl/data_import/doaj.py
@@ -4,23 +4,17 @@ import json
 import uuid
 
 
-# {
-#     "url": "http://www.akademos.asm.md/files/O%20noua%20platforma%20stratefica%20pentru%20comunitatea%20stiintifica%20din%20RM.pdf",
-#     "title": "A NEW STRATEGIC PLATFORM FOR THE SCIENTIFIC COMMUNITY OF MOLDOVA",
-#     "journal": "Akademos: Revista de Ştiinţă, Inovare, Cultură şi Artă",
-#     "language": ["EN", "RO", "RU"],
-#     "subject": ["Arts in general", "Science"],
-# }
-
-
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Transfer doaj data to a common format"
     )
-
     parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
-    parser.add_argument("-i", "--input_path", help="verbose output")
-    parser.add_argument("-o", "--output_path", help="verbose output")
+    parser.add_argument(
+        "-i", "--input_path", help="path to input file"
+    )  # Correct help text
+    parser.add_argument(
+        "-o", "--output_path", help="path to output file"
+    )  # Correct help text
 
     args = parser.parse_args()
     return args
@@ -35,15 +29,14 @@ def main():
             line_data = json.loads(line)
             line_id = uuid.uuid5(uuid.NAMESPACE_URL, line_data["url"]).hex
 
-            language = line_data["language"]
-            if isinstance(language, (list, set)):
-                for l in language:
-                    if l.lower() == "en":
-                        language = "en"
-
-                if language != "en":
-                    print(f"Unknown language: {line_data}")
-                    # exit(1)
+            language = "unknown"
+            if isinstance(line_data.get("language", []), (list, set)):
+                if "EN" in [l.upper() for l in line_data["language"]]:
+                    language = "en"
+                if "DE" in [l.upper() for l in line_data["language"]]:
+                    language = "de"
+                else:
+                    continue
 
             results.append(
                 {
@@ -58,7 +51,7 @@ def main():
                             "content": line_data["title"],
                             "page": 0,
                             "type": "title",
-                            "language": language.lower(),
+                            "language": language,
                         },
                     ],
                 }
@@ -68,8 +61,4 @@ def main():
         for r in results:
             f.write(json.dumps(r) + "\n")
 
-    return 0
-
-
-if __name__ == "__main__":
-    sys.exit(main())
+    return
