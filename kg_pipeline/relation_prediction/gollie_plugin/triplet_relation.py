734e51c3ade70b0c6fcd935af2019c25b699b4dd
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-04-11 11:37:18 +0200

    Add Gollie Dataclasses

--- kg_pipeline/relation_prediction/gollie_plugin/triplet_relation.py
+++ kg_pipeline/relation_prediction/gollie_plugin/triplet_relation.py
@@ -26,14 +26,32 @@ class CreatorRelation(Template):
 
 @dataclass
 class PaintingDepictsRelation(Template):
-    """Name of the painter of a painting."""
+    """The object the painting depicts."""
 
     painting: str  # The name of the painting, i.e. Mona Lisa
     depicts: str  # The name of an object shown in the painting
+
+
+@dataclass
+class LocationCreationRelation(Template):
+    """Name of the location the painting was created."""
+
+    painting: str  # The name of the painting, i.e. Mona Lisa
+    creator: str  # The name of the location, i.e. Florence
+
+
+@dataclass
+class MovementRelation(Template):
+    """The movement with which the painting is associated."""
+
+    painting: str  # The name of the painting, i.e. Mona Lisa
+    movement: str  # The name of the movement, i.e. Renaissance
 
 
 ENTITY_DEFINITIONS: List[Template] = [
     InceptionRelation,
     CreatorRelation,
     PaintingDepictsRelation,
+    LocationCreationRelation,
+    MovementRelation,
 ]
