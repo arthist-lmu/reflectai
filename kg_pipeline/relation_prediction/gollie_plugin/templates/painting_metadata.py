dc5d4b5b644ac88f746427b1f1601ae78e9b95f2
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-04-17 11:23:33 +0200

    Added New Dataclasses Metadata

--- kg_pipeline/relation_prediction/gollie_plugin/templates/painting_metadata.py
+++ kg_pipeline/relation_prediction/gollie_plugin/templates/painting_metadata.py
@@ -43,6 +43,26 @@ class PaintingMaterial(Template):
     painting: str  # The name of the painting, i.e. Mona Lisa
     material: str  # The material of a painting, i.e. oil on canvas
 
+@dataclass
+class AliasNames(Template):
+    """Alias names for artworks refer to alternative titles or nicknames that a piece of art 
+    may acquire beyond its official title. These alternative names often arise from the 
+    public, critics, or the artists themselves and can reflect popular interpretations, 
+    striking features, or emotional responses elicited by the artwork."""
+
+    painting: str  # The name of the painting, i.e. Mona Lisa
+    alias: str  # The alias name of a painting, i.e. La Gioconda
+
+@dataclass
+class TitleLanguages(Template):
+    """Names of artworks in different languages represent the various translations or 
+    adaptations of an artwork's title across cultural and linguistic boundaries."""
+
+    painting: str  # The name of the painting, i.e. The Scream
+    GermanName: str  # The German name of a painting, Der Schrei
+    FrenchName: str # The French name of a painting, Le Cri
+    EnglishName: str # The English name of painting, The Scream
+
 
 @dataclass
 class MovementRelation(Template):
@@ -69,6 +89,8 @@ ENTITY_DEFINITIONS: List[Template] = [
     LocationCreationRelation,
     InceptionRelation,
     PaintingMaterial,
+    AliasNames,
+    TitleLanguages,
     MovementRelation,
     LocationDisplayed,
 ]
