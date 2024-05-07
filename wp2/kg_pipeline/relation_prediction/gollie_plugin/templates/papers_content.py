14d9365894b9b691a96d393d6ca8d5118f4c878a
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-05-07 13:58:09 +0200

    new papers_content

--- wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/papers_content.py
+++ wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/papers_content.py
@@ -10,17 +10,21 @@ Relation definitions
 
 @dataclass
 class NamesMentioned(Template):
-    """Names of people, researchers, artists, writer, philosophers or other people which are mentioned in the article. They could be quoted or
-    cited in order to enrich the text.
+    """Names which are mentioned in the article. They could be quoted or cited in order to enrich the text.
     """
 
     name: str  # The name of the researcher, artist, writer, philosopher who was quoted in the article
 
 @dataclass
+class ArtistsMentioned(Template):
+    """Names of other artists that are mentioned in the article. 
+    """
+
+    name: str  # The name of the mentioned artists i.e. Leonardo da Vinci, Michelangelo, van Gogh
+
+@dataclass
 class MuseumsMentioned(Template):
-    """Names of museums which are mentioned in the article. They could have acquired the artworks or added
-    them to their collection. Museums are places and locations that collect artworks and put them into
-    exhibitions. 
+    """Names of museums that are mentioned in the article. Museums are places and locations that collect artworks.
     """
 
     name: str  # The name of the museum, i.e. Louvre, Tate Gallery, Smithsonian
@@ -37,6 +41,7 @@ class ArtworksMentioned(Template):
 
 ENTITY_DEFINITIONS: List[Template] = [
     NamesMentioned,
+    ArtistsMentioned,
     ArtworksMentioned,
     MuseumsMentioned,
 ]
@@ -44,7 +49,7 @@ ENTITY_DEFINITIONS: List[Template] = [
 
 ENTITY_PARSER = {}
 
-def name_mentioned_relation_to_triplet(package: NamesMentioned):
+def name_mentioned_relation_to_triplet(package: NamesMentioned) -> List:
     triplets = [
         {
             "subject": {
