787c341a0406cd54a1660f7b850e963f259f4d95
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-05-10 10:31:52 +0200

    Update museum_mentions.py

--- wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/museum_mentions.py
+++ wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/museum_mentions.py
@@ -21,8 +21,8 @@ class AuctionHousesMentioned(Template):
     """An auction house that is mentioned in the article
     """
 
+    artwork: str # The name of the artwork mentioned
     name: str  # The name of the auctionhouse mentioned
-    location: str # The location of the auctionhouse
     
 
 
@@ -31,7 +31,43 @@ ENTITY_DEFINITIONS: List[Template] = [
     AuctionHousesMentioned,
 ]
 
-def name_mentioned_relation_to_triplet(package: MuseumsMentioned) -> List:
+def museum_mentioned_to_triplet(package: MuseumsMentioned) -> List:
+    triplets = [
+        {
+            "subject": {
+                "label": package.artwork,
+            },
+            "relation": {
+                "label": "displayed",
+                "wikidata_id": "P276",
+            },
+            "object": {
+                "label": package.name,
+            },
+        }
+    ]
+
+    return triplets
+
+def museum_location_to_triplet(package: MuseumsMentioned) -> List:
+    triplets = [
+        {
+            "subject": {
+                "label": package.museum,
+            },
+            "relation": {
+                "label": "displayed",
+                "wikidata_id": "P276",
+            },
+            "object": {
+                "label": package.location,
+            },
+        }
+    ]
+
+    return triplets
+
+def auctionhouse_mentioned_to_triplet(package: AuctionHousesMentioned) -> List:
     triplets = [
         {
             "subject": {
@@ -50,6 +86,7 @@ def name_mentioned_relation_to_triplet(package: MuseumsMentioned) -> List:
     return triplets
 
 ENTITY_PARSER: Dict = {
-    MuseumsMentioned.__name__: name_mentioned_relation_to_triplet
-
+    MuseumsMentioned.__name__: museum_mentioned_to_triplet,
+    MuseumsMentioned.__name__: museum_location_to_triplet,
+    AuctionHousesMentioned.__name__: auctionhouse_mentioned_to_triplet, 
 }
