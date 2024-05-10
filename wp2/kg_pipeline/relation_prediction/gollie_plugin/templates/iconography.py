ebbcef6cf1b14e224866b34ba21c4b3494445d7b
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-05-10 12:00:55 +0200

    Update iconography.py

--- wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/iconography.py
+++ wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/iconography.py
@@ -22,10 +22,17 @@ class ReligiousFigures(Template):
     painting: str  # The name of the painting, i.e. Mona Lisa
     figure: list  # The figures depicted in the painting, i.e. Jesus, Mary Magdalene, Angel etc.
 
+@dataclass
+class MythologicalFigures(Template):
+    """Mythological Figrues depicted in the painting which are part of the content. These  figures could depict scenes from Greek and Roman Mythologies."""
+
+    painting: str  # The name of the painting, i.e. Mona Lisa
+    figure: list  # The figures depicted in the painting, i.e. Jesus, Mary Magdalene, Angel etc.
 
 ENTITY_DEFINITIONS: List[Template] = [
     Attributes,
     ReligiousFigures,
+    MythologicalFigures,
 ]
 
 
@@ -65,7 +72,26 @@ def ReligiousFigures_relation_to_triplet(package: Attributes):
 
     return triplets
 
+def MythologicalFigures_relation_to_triplet(package: MythologicalFigures):
+    triplets = [
+        {
+            "subject": {
+                "label": package.painting,
+            },
+            "relation": {
+                "label": "made from material",
+                "wikidata_id": "P186",
+            },
+            "object": {
+                "label": package.figure,
+            },
+        }
+    ]
+
+    return triplets
+
 ENTITY_PARSER = {
     Attributes.__name__: Attributes_relation_to_triplet,
     ReligiousFigures.__name__: ReligiousFigures_relation_to_triplet
+    MythologicalFigures.__name__: MythologicalFigures_relation_to_triplet
 }
