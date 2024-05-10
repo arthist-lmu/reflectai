7545c6e4307b9d5580709dce3ef4cef429492a54
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-05-10 11:50:49 +0200

    Adding iconography dataclasses

--- wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/iconography.py
+++ wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/iconography.py
@@ -15,9 +15,17 @@ class Attributes(Template):
     painting: str  # The name of the painting, i.e. Mona Lisa
     objects: list  # The objects depicted in the painting
 
+@dataclass
+class ReligiousFigures(Template):
+    """Religious Figrues depicted in the painting"""
+
+    painting: str  # The name of the painting, i.e. Mona Lisa
+    figure: list  # The figures depicted in the painting, i.e. Jesus, Mary Magdalene, Angel etc.
+
 
 ENTITY_DEFINITIONS: List[Template] = [
     Attributes,
+    ReligiousFigures,
 ]
 
 
@@ -39,6 +47,25 @@ def Attributes_relation_to_triplet(package: Attributes):
 
     return triplets
 
+def ReligiousFigures_relation_to_triplet(package: Attributes):
+    triplets = [
+        {
+            "subject": {
+                "label": package.painting,
+            },
+            "relation": {
+                "label": "made from material",
+                "wikidata_id": "P186",
+            },
+            "object": {
+                "label": package.figure,
+            },
+        }
+    ]
+
+    return triplets
+
 ENTITY_PARSER = {
     Attributes.__name__: Attributes_relation_to_triplet,
+    ReligiousFigures.__name__: ReligiousFigures_relation_to_triplet
 }
