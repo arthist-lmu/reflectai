1dffc776c167f6ef5f7214a442611dfc754c92b4
julianstalter <78112604+julianstalter@users.noreply.github.com>
2024-05-29 10:38:27 +0200

    add allegory dataclass to gollie: iconography

--- wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/iconography.py
+++ wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/iconography.py
@@ -13,26 +13,42 @@ class Attributes(Template):
     """Objects depicted in the painting that have symbolic value like a key or a tool used for certain professions"""
 
     painting: str  # The name of the painting, i.e. The Last Supper
-    objects: List[str]  # The objects depicted in the painting
+    objects: List[str]  # The objects depicted in the painting, i.e. key, sword, scale
+
 
 @dataclass
 class ReligiousFigures(Template):
     """Religious Figures depicted in the painting which are part of the content. These religious figures could depict scenes from the Bible or from martyr stories."""
 
     painting: str  # The name of the painting, i.e. The Last Supper
-    figure: List[str]  # The figures depicted in the painting, i.e. Jesus, Mary Magdalene, Angel etc.
+    figure: List[
+        str
+    ]  # The figures depicted in the painting, i.e. Jesus, Mary Magdalene, Angel etc.
+
 
 @dataclass
 class MythologicalFigures(Template):
     """Mythological Figures depicted in the painting which are part of the content. These  figures could depict scenes from Greek and Roman Mythologies."""
 
     painting: str  # The name of the painting, i.e. The Last Supper
-    figure: list  # The figures depicted in the painting, i.e. Venus, Volcan, Jupyter
+    figure: List[
+        str
+    ]  # The figures depicted in the painting, i.e. Venus, Volcan, Jupyter
+
+
+@dataclass
+class Allegory(Template):
+    """visual representation in which a character, place, or event can be interpreted to represent a meaning with moral or political significance"""
+
+    painting: str  # The name of the painting, i.e. The Last Supper
+    allegory: str  # The name of the allegory, i.e. Allegory of Fortune, Allegory of Justice, Allegory of Time
+
 
 ENTITY_DEFINITIONS: List[Template] = [
     Attributes,
     ReligiousFigures,
     MythologicalFigures,
+    Allegory,
 ]
 
 
@@ -54,6 +70,7 @@ def Attributes_relation_to_triplet(package: Attributes):
 
     return triplets
 
+
 def ReligiousFigures_relation_to_triplet(package: ReligiousFigures):
     triplets = [
         {
@@ -67,10 +84,12 @@ def ReligiousFigures_relation_to_triplet(package: ReligiousFigures):
             "object": {
                 "label": x,
             },
-        } for x in package.figure
+        }
+        for x in package.figure
     ]
 
     return triplets
+
 
 def MythologicalFigures_relation_to_triplet(package: MythologicalFigures):
     triplets = [
@@ -83,15 +102,35 @@ def MythologicalFigures_relation_to_triplet(package: MythologicalFigures):
                 "wikidata_id": "P1354",
             },
             "object": {
-                "label": package.figure,
+                "label": x,
             },
         }
+        for x in package.figure
+    ]
+
+
+def Allegory_relation_to_triplet(package: Allegory):
+    triplets = [
+        {
+            "subject": {
+                "label": package.painting,
+            },
+            "relation": {
+                "label": "depicts person",
+                "wikidata_id": "P1354",
+            },
+            "object": {
+                "label": x,
+            },
+        }
+        for x in package.allegory
     ]
 
     return triplets
 
+
 ENTITY_PARSER = {
     Attributes.__name__: Attributes_relation_to_triplet,
     ReligiousFigures.__name__: ReligiousFigures_relation_to_triplet,
-    MythologicalFigures.__name__: MythologicalFigures_relation_to_triplet
+    MythologicalFigures.__name__: MythologicalFigures_relation_to_triplet,
 }
