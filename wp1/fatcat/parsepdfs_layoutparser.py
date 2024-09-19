8ec3e26e35bf0476bd387fc099483f7728b89a30
Elias Entrup <elias.entrup@tib.eu>
2024-09-19 09:36:15 +0200

    wp1/parsepdfs: use only one layer of nesting for image directories

--- wp1/fatcat/parsepdfs_layoutparser.py
+++ wp1/fatcat/parsepdfs_layoutparser.py
@@ -51,7 +51,7 @@ def parse_image(model: lp.models.base_layoutmodel.BaseLayoutModel, lang_pipeline
         for figure, caption in figure_blocks:
             cropped = figure.crop_image(image)
             fig_id = hashlib.sha256(cropped.tobytes()).hexdigest()[:32]
-            img_path = (image_folder / fig_id[:2] / fig_id[2:4])
+            img_path = image_folder / fig_id[:2]
             img_path.mkdir(exist_ok=True, parents=True)
             img_path = img_path / (fig_id + '.jpg')
             Image.fromarray(cropped).save(img_path)
