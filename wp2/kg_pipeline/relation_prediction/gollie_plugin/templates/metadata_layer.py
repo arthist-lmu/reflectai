a4805a5ece2106c5e795bd189258244406e0e344
julianstalter <78112604+julianstalter@users.noreply.github.com>
2025-01-13 11:14:12 +0100

    ArtGenre Relation

--- wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/metadata_layer.py
+++ wp2/kg_pipeline/relation_prediction/gollie_plugin/templates/metadata_layer.py
@@ -7,55 +7,40 @@ from ..utils_typing import Generic as Template
 Relation definitions
 """
 
-
 @dataclass
-class CreatorRelation(Template):
-    """The name or pseudonym of the painter that created the painting. The creator is involved in 
-    the conception and creation of artwork through various techniques and mediums including oil, 
-    acrylic, watercolor, frescoes, and more.Their work is not limited to just creating paintings; 
-    many painters also delve into the realms of drawing, sculpting, and other forms of visual 
-    art expressions.
+class ArtGenre(Template):
+    """
+    form of art in terms of a medium, format, or theme
     """
 
-    painting: str  # The name of the painting, Such as Mona Lisa, Starry Night, Guernica, The Scream
-    creator: str  # The name of the painter, i.e. Leonardo da Vinci, Vincent van Gogh, Pablo Picasso, Edvard Munch
-    workshop: str  # The name of the workshop
+    genre: str #Description of genre like pre-impressionistic or 19th-century style
 
 @dataclass
-class LocationCreationRelation(Template):
-    """The name of the location where the painting was created"""
+class ArtMaterial(Template):
+    """
+    substance, raw ingredient, or tool that is utilized by an artist to create a work of art
+    """
 
-    painting: str  # The name of the painting, i.e. Mona Lisa, Starry Night, Guernica, The Scream
-    location: str  # The name of the location, i.e. Florence, Provence, Paris, Oslo
-
-
-@dataclass
-class InceptionRelation(Template):
-    """The Year when an painting was painted or created. The creation date of a painting refers to the specific year or range of years
-    during which the artwork was produced."""
-
-    painting: str  # The name of the painting, i.e. Mona Lisa, Starry Night, Guernica, The Scream
-    date: int  # The year in which the picture was painted or created
+    material: str #Description of Material such as Oil on Canvas, Drawing, Photography, Woodcut
 
 
 ENTITY_DEFINITIONS: List[Template] = [
-    CreatorRelation,
-    LocationCreationRelation,
-    InceptionRelation,
+    ArtGenre,
+    ArtMaterial,
 ]
 
-def creator_relation_to_triplet(package: CreatorRelation):
+def ArtGenre_relation_to_triplet(package: ArtGenre):
     triplets = [
         {
             "subject": {
                 "label": package.painting,
             },
             "relation": {
-                "label": "creator",
-                "wikidata_id": "wdt:P170",
+                "label": "has genre",
+                "wikidata_id": "wdt:Q1792379",
             },
             "object": {
-                "label": package.creator,
+                "label": package.ArtGenre,
             },
         }
     ]
@@ -63,30 +48,7 @@ def creator_relation_to_triplet(package: CreatorRelation):
     return triplets
 
 
-#def location_creation_relation_to_triplet(package: LocationCreationRelation):
-    triplets = [
-        {
-            "subject": {
-                "label": package.painting,
-            },
-            "relation": {
-                "label": "location of creation",
-                "wikidata_id": "wdt:P1071",
-            },
-            "object": {
-                "label": package.location,
-            },
-        }
-    ]
-
-    return triplets
-
-
-def inception_relation_to_triplet(package: InceptionRelation):
-    date = package.date
-    if isinstance(package.date, int):
-        date = str(package.date)
-
+def ArtMaterial_relation_to_triplet(package: ArtMaterial):
     triplets = [
         {
             "subject": {
@@ -94,10 +56,10 @@ def inception_relation_to_triplet(package: InceptionRelation):
             },
             "relation": {
                 "label": "inception",
-                "wikidata_id": "wdt:P571",
+                "wikidata_id": "wdt:Q15303351",
             },
             "object": {
-                "label": date,
+                "label": package.ArtMaterial,
             },
         }
     ]
@@ -106,7 +68,6 @@ def inception_relation_to_triplet(package: InceptionRelation):
 
 
 ENTITY_PARSER = {
-    CreatorRelation.__name__: creator_relation_to_triplet,
-    #LocationCreationRelation.__name__: location_creation_relation_to_triplet,
-    InceptionRelation.__name__: inception_relation_to_triplet,
+    ArtGenre.__name__: ArtGenre_relation_to_triplet,
+    ArtMaterial.__name__: ArtMaterial_relation_to_triplet,
 }
